7a7c1467f289f0e3b717df62d4022c1249c46761
Luis Carlos Eich <pelinche@gmail.com>
2021-02-10 12:34:59 -0300

    NextJs_Example

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home1x</h1>
+      <Contador />
+      <div>Teste </div>
+    </div>
+  );
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1);
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
+
+  return (
+    <div>
+      <p>{contador}</p>
+      <button onClick={adicionarContador}>Clicar</button>
+    </div>
+  );
+}
+
+export default Home;
